30bdf32bc6f62149f8cdb9430c357e257b8ff5b4
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 00:06:15 +0300

    second step

--- Test/src/ChatBot/ChatBot.java
+++ Test/src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
         public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
             System.out.println("Hello! My name is Alisa.\n" +
-                    "I was created in 2021.");
+                    "I was created in 2021.\n" +
+                    "Please, remind me your name.");
+            String name = in.next();
+            System.out.println("What a great name you have, " + name);
         }
 }
