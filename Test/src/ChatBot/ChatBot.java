0e8a4112db42a898cb5f19b0eecd4af03c4c7780
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 00:09:03 +0300

    third step

--- Test/src/ChatBot/ChatBot.java
+++ Test/src/ChatBot/ChatBot.java
@@ -9,6 +9,12 @@ public class ChatBot {
                     "I was created in 2021.\n" +
                     "Please, remind me your name.");
             String name = in.next();
-            System.out.println("What a great name you have, " + name);
+            System.out.println("What a great name you have, " + name + "\nLet me guess your age.\n" +
+                    "Enter remainders of dividing your age by 3, 5 and 7.");
+            int remainder3 = in.nextInt();
+            int remainder5 = in.nextInt();
+            int remainder7 = in.nextInt();
+            int remainder = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+            System.out.println("Your age is " + remainder + " that&#39;s a good time to start programming!");
         }
 }
