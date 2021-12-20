c2c5da069791bc6331617787aa0ce756380f3a8e
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 00:04:22 +0300

    first step

---
+++ Test/src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+        public static void main(String[] args){
+            System.out.println("Hello! My name is Alisa.\n" +
+                    "I was created in 2021.");
+        }
+}
