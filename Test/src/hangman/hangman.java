3b28e38d242364401e5750f4722e57e7c6ead554
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 17:21:41 +0300

    Third step hangman

--- Test/src/hangman/hangman.java
+++ Test/src/hangman/hangman.java
@@ -1,13 +1,28 @@
 package hangman;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Math.random;
 
 public class hangman {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+
+        ArrayList<String> word = new ArrayList<String>();
+        word.add("java");
+        word.add("kotlin");
+        word.add("python");
+        word.add("javascript");
+        int min = 0;
+        int max = 3;
+
+        int rand = min + (int) (random() * (max-1+min));
+        String final_word = word.get(rand);
         System.out.println("Guest the word");
         String name = in.next();
-        if (name.equals("java")){
+        if (name.equals(final_word)){
             System.out.println("YOU WIN");
         }
         else {
