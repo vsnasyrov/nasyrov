aae3d7ab226832a686f44c6ed25d9d3924c5da8c
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 18:41:47 +0300

    1 step

---
+++ Test/src/TicTacToe/TicTacToe.java
@@ -0,0 +1,12 @@
+package TicTacToe;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
