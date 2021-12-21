5635acc7528afba2513355261052bd05b0f2a29d
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 17:50:01 +0300

    7 step hangman

--- Test/src/hangman/hangman.java
+++ Test/src/hangman/hangman.java
@@ -19,31 +19,49 @@ public class hangman {
         int max = 3;
         char name2;
         int endgame = 0;
+        char[] all = new char[32];
+        int all_test = 0;
         int rand = min + (int) (random() * (max-1+min));
         String final_word = word.get(rand);
+        int number = 0;
+        int change;
         char[] name = new char[final_word.length()];
         for (int i = 0; i < final_word.length(); i++){
             name[i] = '-';
         }
         for (int i = 0; i < 8; i++) {
             endgame = 0;
-            System.out.println("\nGuest the word");
+            change = 0;
+            all_test = 0;
+            System.out.println("\nGuest the word ");
             name2 = in.next().charAt(0);
             for (int i1 = 0; i1 < final_word.length(); i1++){
                 if (name2 == final_word.charAt(i1)){
                     if (name[i1] == name2){
-                        i++;
-                        System.out.println("No Improvements");
+                        System.out.print("You've already guessed this letter.\n");
                     }
                     name[i1] = name2;
                     i--;
+                    change++;
                 }
-                else{
-                    System.out.println("That letter doesn't appear in the word");
-                }
+
                 System.out.print(name[i1]);
                 if (name[i1] != '-'){
                     endgame++;
+                }
+            }
+            if (change == 0) {
+                for (int i2 = 0; i2 < 32; i2++) {
+                    if (all[i2] == name2) {
+                        all_test++;
+                    }
+                }
+                if (all_test > 0) {
+                    System.out.println("\nYou've already guessed this letter.");
+                } else {
+                    System.out.println("\nThat letter doesn't appear in the word");
+                    all[number] = name2;
+                    number++;
                 }
             }
             if (endgame == final_word.length()) {
