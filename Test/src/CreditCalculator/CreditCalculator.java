7d71aa13008a3efb4d16fd2db889dc379529ee53
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 21:46:36 +0300

    4 step

--- Test/src/CreditCalculator/CreditCalculator.java
+++ Test/src/CreditCalculator/CreditCalculator.java
@@ -13,7 +13,7 @@ public class CreditCalculator {
                 "Month 2: repaid 250\n" +
                 "Month 3: repaid 500\n" +
                 "The loan has been repaid!");
-        System.out.println("a/n/p?");
+        System.out.println("a/n/p/d?");
         String enter = in.next();
         if (enter.charAt(0) == 'a') {
             System.out.println("Loan: ");
@@ -24,8 +24,7 @@ public class CreditCalculator {
             System.out.println("principal: ");
             int principal = in.nextInt();
             System.out.println("You must paying per month:  " + (Math.ceil(principal * (loan * pow(1 + loan, months)) / (pow(1 + loan, months) - 1))));
-        }
-        else if(enter.charAt(0) == 'n'){
+        } else if (enter.charAt(0) == 'n') {
             System.out.println("Loan: ");
             double loan = in.nextDouble();
             loan = loan / (12 * 100);
@@ -33,13 +32,12 @@ public class CreditCalculator {
             int payment = in.nextInt();
             System.out.println("principal: ");
             int principal = in.nextInt();
-            System.out.print("You must paying for " + (Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan))/12) + " Years");
-            if ((Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan))) %12 > 0){
-                System.out.print(" and " + ((Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan)))-
-                        (Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan))/12)) + " Months");
+            System.out.print("You must paying for " + (Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan)) / 12) + " Years");
+            if ((Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan))) % 12 > 0) {
+                System.out.print(" and " + ((Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan))) -
+                        (Math.ceil(log(payment / (payment - loan * principal)) / log(1 + loan)) / 12)) + " Months");
             }
-        }
-        else if (enter.charAt(0) == 'p'){
+        } else if (enter.charAt(0) == 'p') {
             System.out.println("Loan: ");
             double loan = in.nextDouble();
             loan = loan / (12 * 100);
@@ -49,9 +47,24 @@ public class CreditCalculator {
             int months = in.nextInt();
             System.out.println("Principal: " + (Math.ceil(payment / ((loan * pow(1 + loan, months)) / (pow(1 + loan, months) - 1)))));
 
-        }
-        else {
-            System.out.println("Pls take a/n/p");
+        } else if (enter.charAt(0) == 'd') {
+            double over = 0;
+            System.out.println("Loan: ");
+            double loan = in.nextDouble();
+            loan = loan / (12 * 100);
+            System.out.println("principal: ");
+            int principal = in.nextInt();
+            System.out.println("months: ");
+            int months = in.nextInt();
+            for (int i = 1; i < months + 1; i++) {
+                System.out.println("d = " + (Math.ceil(principal / months + loan * (principal - (principal * (i - 1)) / months))));
+                over = over + (Math.ceil(principal / months + loan * (principal - (principal * (i - 1)) / months)) - (principal / months));
+                if (i == months) {
+                    System.out.println(over);
+                }
+            }
+        } else {
+            System.out.println("Pls take a/n/p/d");
         }
 
     }
