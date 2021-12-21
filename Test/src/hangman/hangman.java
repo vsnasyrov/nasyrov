2fb03dcd9685e863c09ad364c2ab97a54ecd748d
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-21 17:33:48 +0300

    5 step hangman

--- Test/src/hangman/hangman.java
+++ Test/src/hangman/hangman.java
@@ -17,24 +17,33 @@ public class hangman {
         word.add("javascript");
         int min = 0;
         int max = 3;
-
+        char name2;
+        int endgame = 0;
         int rand = min + (int) (random() * (max-1+min));
         String final_word = word.get(rand);
-        for (int i =0; i < final_word.length(); i++){
-            if (i < 2){
-                System.out.print(final_word.charAt(i));
+        char[] name = new char[final_word.length()];
+        for (int i = 0; i < final_word.length(); i++){
+            name[i] = '-';
+        }
+        for (int i = 0; i < 8; i++) {
+            endgame = 0;
+            System.out.println("\nGuest the word");
+            name2 = in.next().charAt(0);
+            for (int i1 = 0; i1 < final_word.length(); i1++){
+                if (name2 == final_word.charAt(i1)){
+                    name[i1] = name2;
+                }
+                System.out.print(name[i1]);
+                if (name[i1] != '-'){
+                    endgame++;
+                }
             }
-            else{
-                System.out.print("-");
+            if (endgame == final_word.length()) {
+                System.out.println("\nYOU WIN");
+                System.exit(0);
             }
         }
-        System.out.println("\nGuest the word");
-        String name = in.next();
-        if (name.equals(final_word)){
-            System.out.println("YOU WIN");
-        }
-        else {
-            System.out.println("YOU LOSE");
-        }
+        System.out.println("\nYOU LOSE");
+        System.exit(0);
     }
 }
