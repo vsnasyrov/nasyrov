590a7fb5779953c4dff431fe8a0c3627720a1dd0
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-23 20:23:44 +0300

    6 step

--- Matrix_final/src/MatrixProcessing.java
+++ Matrix_final/src/MatrixProcessing.java
@@ -18,7 +18,7 @@ public class MatrixProcessing {
             }
             int c = 0;
 
-            int[][] matrix2 = new int[a1][b1];
+            double[][] matrix2 = new double[a1][b1];
             for (int i = 0; i < a1; i++) {
                 for (int i1 = 0; i1 < b1; i1++) {
                     matrix2[i][i1] = matrix[c];
@@ -60,7 +60,7 @@ public class MatrixProcessing {
                         matrix3[i][i1] = in.nextInt();
                 }
                 if (b1 == a2) {
-                    int[][] matrix4 = new int[a1][b2];
+                    double[][] matrix4 = new double[a1][b2];
                     for (int i = 0; i < a1; i++) {
                         for (int i1 = 0; i1 < b2; i1++)
                             matrix3[i][i1] = 0;
@@ -87,7 +87,7 @@ public class MatrixProcessing {
                         "Horizontal line(h)");
                 char type = in.next().charAt(0);
                 if (type == 'm'){
-                    int[][] matrix3 = new int[b1][a1];
+                    double[][] matrix3 = new double[b1][a1];
 
                     for (int i = 0; i < b1; i++) {
                         for (int i1 = 0; i1 < a1; i1++) {
@@ -97,7 +97,7 @@ public class MatrixProcessing {
                         System.out.println();
                     }
                 }else if(type == 's'){
-                    int[][] matrix3 = new int[b1][a1];
+                    double[][] matrix3 = new double[b1][a1];
                         int c2 = 1;
                         int c3 =1;
                     for (int i = 0; i < b1; i++) {
@@ -113,7 +113,7 @@ public class MatrixProcessing {
                     }
 
                 }else if(type == 'v'){
-                    int[][] matrix3 = new int[a1][b1];
+                    double[][] matrix3 = new double[a1][b1];
                     int c2 = 1;
                     for (int i = 0; i < a1; i++) {
                         for (int i1 = 0; i1 < b1; i1++) {
@@ -127,7 +127,7 @@ public class MatrixProcessing {
                     }
 
                 }else if(type == 'h'){
-                    int[][] matrix3 = new int[a1][b1];
+                    double[][] matrix3 = new double[a1][b1];
                     int c3 =1;
                     for (int i = 0; i < a1; i++) {
                         for (int i1 = 0; i1 < b1; i1++) {
@@ -151,15 +151,66 @@ public class MatrixProcessing {
                     System.out.println("ERROR");
                 }
             }
-            else if (what == 'i'){
+            else if (what == 'i') {
+                if (a1 == b1) {
+                    double t;
+                    int l = a1;
 
+                    double[][] matrix4 = new double[l][l];
+
+                    for (int i = 0; i < l; i++)
+                        for (int i1 = 0; i1 < l; i1++) {
+                            matrix4[i][i1] = 0;
+
+                            if (i == i1)
+                                matrix4[i][i1] = 1;
+                        }
+
+                    for (int i2 = 0; i2 < l; i2++) {
+                        t = matrix2[i2][i2];
+
+                        for (int i3 = 0; i3 < l; i3++) {
+                            matrix2[i2][i3] /= t;
+                            matrix4[i2][i3] /= t;
+                        }
+
+                        for (int i = i2 + 1; i < l; i++) {
+                            t = matrix2[i][i2];
+
+                            for (int i1 = 0; i1 < l; i1++) {
+                                matrix2[i][i1] -= matrix2[i2][i1] * t;
+                                matrix4[i][i2] -= matrix4[i2][i1] * t;
+                            }
+                        }
+                    }
+
+                    for (int i2 = l - 1; i2 > 0; i2--) {
+                        for (int i = i2 - 1; i >= 0; i--) {
+                            t = matrix2[i][i2];
+
+                            for (int i1 = 0; i1 < l; i1++) {
+                                matrix2[i][i1] -= matrix2[i2][i1] * t;
+                                matrix4[i][i1] -= matrix4[i2][i1] * t;
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < l; i++)
+                        for (int i1 = 0; i1 < l; i1++)
+                            matrix2[i][i1] = matrix4[i][i1];
+
+
+                }
+            }
+            else {
+                System.out.println("ERROR");
             }
 
         } while(true);
     }
-    public static void d (int[][] matrix2, double m){
+    public static void d (double[][] matrix2, double m){
         if (matrix2.length > 1){
-            int [][] matrix3 = new int[matrix2.length - 1][matrix2[0].length - 1];
+            double [][] matrix3 = new double[matrix2.length - 1][matrix2[0].length - 1];
             for (int i = 0; i < matrix2[0].length; i++) {
                 for (int i1 = 1; i1 < matrix2.length; i1++) {
                     for (int i2 = 0; i2 < matrix2[0].length; i2++) {
