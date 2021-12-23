f274121b782699c385be07ba5da8441c52815ced
Valera Nasyrov <v.s.nasyrov@student.khai.edu>
2021-12-23 21:08:39 +0300

    test.java added to branch

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main (String[] args){
-        System.out.println("education");
-        //new string
-        System.out.println("DICT");
+        System.out.println("JAVA");
     }
 }
